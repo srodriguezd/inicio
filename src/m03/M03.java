c228e1081523491d5dcd19c3eb7ae310ab9e3d6f
Pc <Pc@Pac>
2014-09-26 17:39:08 +0200

    commit inicial1

--- src/m03/M03.java
+++ src/m03/M03.java
@@ -23,6 +23,9 @@ public class M03 {
         resultado = n1 + n2;
         
         System.out.println("El resultado es:" + resultado);
+        
+        n1 = 8;
+                
     }
     
 }
--- src/m03/f01.java
+++ src/m03/f01.java
@@ -4,11 +4,30 @@
  * and open the template in the editor.
  */
 package m03;
-
+import java.util.Scanner;
 /**
  *
  * @author Pc
  */
 public class f01 {
+   
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner (System.in);
+
+        int n1;
+        int n2;
+        int resultado;
     
+        System.out.println("Introduce el primer numero");
+        n1 = teclado.nextInt();
+    
+        System.out.println("Introduce el segundo numero");
+        n2 = teclado.nextInt();
+
+        resultado = n1 + n2;
+
+        System.out.println("El resultado es:" + resultado);
+    
+    }
+
 }
